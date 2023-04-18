276e42f9f0f65493b8a482b00ed3ab34bc9e18aa
GiorgosPapoutsakis <116210016+GiorgosPapoutsakis@users.noreply.github.com>
2023-04-18 19:00:13 +0300

    Add files via upload

--- assignment-2023-1/lance_williams.py
+++ assignment-2023-1/lance_williams.py
@@ -1,2 +1,16 @@
-pass
-print("test")
+import sys
+
+method = sys.argv[2]
+if method == "simple":
+    print('OK')
+
+allNumbers = []
+with open(sys.argv[1]) as myfile:
+    for line in myfile:
+        allNumbers.extend((int(number) for number in line.split(' ')))
+        print(allNumbers)
+    
+    
+    
+    print(allNumbers)
+
