819bf1bf7f8bfb92c3370fd7ca02f9be9fc79a28
GiorgosPapoutsakis <116210016+GiorgosPapoutsakis@users.noreply.github.com>
2023-04-18 19:57:08 +0300

    day1 upologizw oles tis apostaseis

--- assignment-2023-1/lance_williams.py
+++ assignment-2023-1/lance_williams.py
@@ -4,13 +4,24 @@ method = sys.argv[2]
 if method == "simple":
     print('OK')
 
+#DIAVAZW TA NOUMERA APTO TXT KAI TA VAZW SE MIA LISTA allNumbers
 allNumbers = []
 with open(sys.argv[1]) as myfile:
     for line in myfile:
         allNumbers.extend((int(number) for number in line.split(' ')))
-        print(allNumbers)
     
+print(allNumbers)
+allNumbers.sort()
+print(allNumbers)
+
+apost = []
+for x in allNumbers:
+    katheApost = []
+    for n in allNumbers:
+        if x!=n :
+            katheApost.append(abs(x-n))
+    apost.append(min(katheApost))
     
-    
-    print(allNumbers)
+
+print(apost)
 
